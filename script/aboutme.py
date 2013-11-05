029e45212d32a79f4f4d05e6deaaf72ed4bd20d9
unknown <zhuhuotui@gmail.com>
2013-11-05 12:12:06 +0800

    aboutme

---
+++ script/aboutme.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+import web
+import about
+urls = ('/.*', about.me,
+)
+app = web.application(urls, globals())
+if __name__ == '__main__':
+    app.run()
+else:
+    application = app.wsgifunc()
